dde45bd383d430a86f687a071d0f4ded6c71c6ed
emanovic7 <e_owusu@.live.com>
2019-10-17 18:55:09 -0400

    add create, update, delete, index methods for Channels

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,2 +1,37 @@
 class ChannelsController < ApplicationController
+
+  def index
+    channels = Channel.all
+    render json: channels
+  end
+
+  def create
+   channel = Channel.create(channel_params)
+   if channel.valid?
+     render json: channel
+   else
+     render json: {errors: channel.errors.full_messages}, status: 422
+   end
+ end
+
+ def update
+   channel = Channel.find(params[:id])
+   if channel.update(channel_params)
+     render json: channel
+   else
+     render json: {errors: channel.errors.full_messages}, status: 422
+   end
+ end
+
+ def destroy
+   channel = Channel.find(params[:id])
+   channel.delete
+ end
+
+
+ private
+   def channel_params
+     params.permit(:name, :img)
+   end
+
 end
