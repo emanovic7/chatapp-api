52f835fd29c6279af8b73cd41ac9b0ec56d4493e
emanovic7 <e_owusu@.live.com>
2019-10-18 00:45:25 -0400

    create CRUD methods for messages in messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,37 @@
 class MessagesController < ApplicationController
+
+  def index
+    messages = Message.all
+    render json: messages
+  end
+
+  def create
+   message = Message.create(message_params)
+   if message.valid?
+     render json: message
+   else
+     render json: {errors: message.errors.full_messages}, status: 422
+   end
+ end
+
+ def update
+   message = Message.find(params[:id])
+   if message.update(message_params)
+     render json: message
+   else
+     render json: {errors: message.errors.full_messages}, status: 422
+   end
+ end
+
+ def destroy
+   message = Message.find(params[:id])
+   message.delete
+ end
+
+
+ private
+   def message_params
+     params.permit(:message, :channel_id, :user_id)
+   end
+
 end
